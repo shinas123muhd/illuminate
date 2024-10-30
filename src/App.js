2a056dbbba192bc5cf517d2ae49e9f46c1af7354
Shinas Muhammed <shinasmuhd43@gmail.com>
2024-10-30 23:37:37 +0530

    "remo;ved images"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 
 import './App.css';
-import industryapps from './assets/industryapps.jpg'
-import map from './assets/map.jpg'
-import rsa from './assets/rsa.jpg'
-import  cybrosys from  './assets/cybrosystech.jpg'
-import sacrosys from './assets/sacrosys.png'
-import scanntek from './assets/scanntek-logo.png'
-import wriox from './assets/wriox.png'
-import banner1 from './assets/topleft.jpg'
 import TopPart from './components/TopPart';
 import BottomPart from './components/BottomPart';
 
